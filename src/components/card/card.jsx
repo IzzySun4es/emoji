78b50fd9b468e8a0578f43d893df8a9e95ea883a
Alexandra Chugaenko <sascha281201@gmain.com>
2024-01-31 11:06:58 +0300

    12333

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,10 +1,10 @@
 import './card.scss'
-function Card(){
+function Card({symbol, title, keywords}){
     return <div className='main'>
                 <div className='card'>
-                    <p>symbol</p>
-                    <p className='title'>title</p>
-                    <p className='keywords'>keywords</p>
+                    <p>{symbol}</p>
+                    <p className='title'>{title}</p>
+                    <p className='keywords'>{keywords}</p>
                 </div>
             </div>;
     }
